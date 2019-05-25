6cf7d287d8b67672473d38c92550132c9a5a7a4a
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-25 01:50:01 -0400

    fixes

--- handler.js
+++ handler.js
@@ -2,55 +2,52 @@ import RSS from 'rss';
 import nodeFetch from 'node-fetch';
 
 export const rss = async (event, context) => {
-  console.log('generating RSS');
 
-  const feed = new RSS({
-    title: process.env.DOMAIN,
-    description: 'a blog about programming',
-    feed_url: process.env.RSS_URL,
-    site_url: process.env.SITE_URL,
-    copyright: `2019 ${process.env.SITE_URL}`,
-    language: 'en',
-    categories: ['programming', 'technology', 'blog'],
-    ttl: '60'
-  });
+    const feed = new RSS({
+        title: process.env.DOMAIN,
+        description: process.env.DESCRIPTION,
+        feed_url: process.env.RSS_URL,
+        site_url: process.env.SITE_URL,
+        copyright: process.env.COPYRIGHT,
+        language: process.env.LANG,
+        ttl: process.env.TTL
+    });
 
-  const post = await getPosts(); 
-  
-  post.map( post => {
-  	feed.item({
-		id: post._id,
-		title: post.title,
-		description: post.contentquestion
-	});
-  
-  });
-    
+    const post = await getPosts();
 
-  return {
-    isBase64Encoded: false,
-    statusCode: 200,
-    headers: {
-	'Content-Type': 'application/rss+xml'
-    },
-    body: feed.xml() 
-   
-  };
+    post.map(post => {
+        feed.item({
+            guid: post._id,
+            title: post.title,
+            date: post.publishedAt,
+            url: post.url
+        });
+
+    });
+
+    const xml = feed.xml({ident: true});
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/xml',
+        },
+        body: xml,
+    };
+
 };
 
-
 async function getPosts() {
-  const res =  await nodeFetch(process.env.POST_ENDPOINT)
-        .catch( e => console.log('failed to fetch posts.'));
-  const posts = await res.json().catch(e => console.log('failed to parse json'));
-  return posts.entries.map( post => {
-    return {
-        ...post,
-        url: generatePostUrl(post)
-    };
-  });
+    const res = await nodeFetch(process.env.POST_ENDPOINT)
+        .catch(e => console.log('failed to fetch posts.'));
+    const posts = await res.json().catch(e => console.log('failed to parse json'));
+    return posts.entries.map(post => {
+        return {
+            ...post,
+            url: generatePostUrl(post)
+        };
+    });
 }
 
 const generatePostUrl = (post) => {
-  return `https://${process.env.DOMAIN}/#/posts/${post._id}`;
+    return `https://${process.env.DOMAIN}/#/posts/${post._id}`;
 };
